44770c20d3b2ad5578af24ff6bdee741b2c691a5
nickncb96 <nicholasncb12@hotmail.com>
2024-02-06 02:43:30 +0000

    APi added with functios to fetch recipes and display them

--- assets/Script.js
+++ assets/Script.js
@@ -1,31 +1,21 @@
-//script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const MEAL_DB_API_KEY = .............................
-    const BING_MAPS_API_KEY = "AmN9gnZWL8MOdvtKxP6U7jUfq9s0bgJrzjoS9a4kSn57vFOxx63oMw4HOnXdGbHR"; 
-    const MEAL_DB_API_URL = "https://www.themealdb.com/api/json/v1/";
-    const BING_MAPS_API_URL = "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels/united%20?mapSize=500,400&key=" + BING_MAPS_API_KEY;
+const bingMapsAPIKey = 'AmN9gnZWL8MOdvtKxP6U7jUfq9s0bgJrzjoS9a4kSn57vFOxx63oMw4HOnXdGbHR';
+const themealdbAPIKey = 'https://themealdb.com/api/json/v1/1/list.php?c=list'; // 
 
- 
-  
-    
-    //Bing Maps API
-    function displayMap(location) {
-      fetch(`${BING_MAPS_API_URL}`)
-        .then((response) => response.json())
-        .then((data) => {
-          const coordinates = data.resourceSets[0].resources[0].point.coordinates;
-         
-          console.log("Map Coordinates:", coordinates);
-        })
-        .catch((error) => {
-          console.error("Error fetching map data:", error);
-        });
-    }
-  
-    // Event listener...Wikipedia button
-    document.getElementById("wikiBtn").addEventListener("click", function () {
-      const wikipediaLink = "https://en.wikipedia.org/";
-      window.open(wikipediaLink, "_blank");
+/// Fetch recipes by name 
+async function fetchRecipesByName(name) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${name}&apiKey=${themealdbAPIKey}`);
+  const data = await response.json();
+  return data.meals || [];
+}
+
+/// Display of recipe section
+async function displayRecipesInRecipeSection(name) {
+  const recipes = await fetchRecipesByName(name);
+  if (recipes.length > 0) {
+    recipes.forEach(recipe => {
+      renderRecipeInRecipeSection(recipe);
     });
-  
-    // comme
+  } else {
+    console.error('No recipes found.');
+  }
+}
